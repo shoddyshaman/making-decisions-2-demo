e50f11c11bbd2fe45b8b99b95d4c727672e926ef
shoddyshaman <dhananjoy.tiwari@gmail.com>
2022-01-06 11:45:58 -0700

    demo finished

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log('hello world')
+console.log("hello world");
+
+let backpack = [];
+
+backpack.push("sword", "potion");
+backpack.push("food");
+backpack.push("shield");
+
+// let onBelt = backpack.splice(0,1)
+let onBelt = backpack.shift();
+
+let furCoat = "fur coat";
+
+backpack.push(furCoat);
+
+backpack.pop();
+
+let itemCount = backpack.length;
+
+backpack.push("flint", "blanket", "knife", "toothbrush");
+
+let backpack2 = backpack.splice(3, 3);
+
+// console.log(backpack2[0])
+// console.log(backpack[1])
+
+// console.log("Backpack2: ", backpack2, "Belt: ", onBelt, 'item-count: ',itemCount);
+
+for (let i = 0; i < 3; i++) {
+    console.log(backpack[i])
+}
+
+console.log('--------------')
+for (let i = 0; i < backpack2.length; i++) {
+    console.log(backpack2[i])
+}
+
